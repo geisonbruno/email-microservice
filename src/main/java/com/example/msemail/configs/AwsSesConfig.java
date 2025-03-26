64d95160dcc787713647a25db9afafaa881fe475
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-03-26 15:02:27 -0300

    refactor: atualizado configuração do AWS SES for SDK v2

--- src/main/java/com/example/msemail/configs/AwsSesConfig.java
+++ src/main/java/com/example/msemail/configs/AwsSesConfig.java
@@ -1,12 +1,12 @@
 package com.example.msemail.configs;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesClient;
 
 @Configuration
 public class AwsSesConfig {
@@ -21,12 +21,10 @@ public class AwsSesConfig {
     private String region;
 
     @Bean
-    public AmazonSimpleEmailService amazonSimpleEmailService() {
-        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        return AmazonSimpleEmailServiceClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region)
+    public SesClient sesClient() {
+        return SesClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 }
